4240b159d2d3506670d138c8c0e1aff2b2274ad6
Zhuxuan <zhuxuan@issuebang.com>
2016-11-24 01:12:00 +0800

    lgs11-24-1

--- Lib/Action/WelcomeAction.class.php
+++ Lib/Action/WelcomeAction.class.php
@@ -13,12 +13,35 @@ class WelcomeAction extends Action {
   	$this->display($tpl);
   }
   public function news(){
+    $get = $this->_get();
+    $type = 1;
+    if($get['type']){
+      $type = (int)$get['type'];
+    }
+    $newstype = array('1'=> '集团动态','2'=> '党建工作','3'=> '行业资讯','4'=> '下载中心');
+    $newsdata = M('news')->where("hide = 0 and type_id = $type")->limit(10)->select();
+    if($get['id']){
+      $id = (int)$get['id'];
+      $anew = M('news')->find($id);
+    }else{
+      $anew = $newsdata[0];
+    }
+    $this->assign('anew', $anew);
+    $this->assign('type', $type);
+    $this->assign('newstype', $newstype[$type]);
+    $this->assign('newsdata1', $newsdata);
+    $this->assign('newsdata2', $newsdata);
     $this->assign('news', 'cur');
   	$this->display();
   }
   public function us(){
+    $get = $this->_get();
+    $tpl = 'us';
+    if($get['type']){
+      $tpl = 'us'.$get['type'];
+    }
     $this->assign('us', 'cur');
-  	$this->display();
+  	$this->display($tpl);
   }
   public function hr(){
     $this->assign('hr', 'cur');
