d96ef4e95a6e8b80373d8778c0df5530428350d9
Zhuxuan <zhuxuan@issuebang.com>
2016-11-24 01:30:07 +0800

    lgs11-24-2

--- Lib/Action/WelcomeAction.class.php
+++ Lib/Action/WelcomeAction.class.php
@@ -1,6 +1,13 @@
 <?php
 class WelcomeAction extends Action {
   public function index(){
+    $newsdata1 = M('news')->where("hide = 0 and type_id = 1")->order('created_time desc')->limit(5)->select();
+    $newsdata2 = M('news')->where("hide = 0 and type_id = 2")->order('created_time desc')->limit(5)->select();
+    $newsdata3 = M('news')->where("hide = 0 and type_id = 3")->order('created_time desc')->limit(5)->select();
+
+    $this->assign('newsdata1', $newsdata1);
+    $this->assign('newsdata2', $newsdata2);
+    $this->assign('newsdata3', $newsdata3);
   	$this->display();
   }
   public function work(){
@@ -19,7 +26,7 @@ class WelcomeAction extends Action {
       $type = (int)$get['type'];
     }
     $newstype = array('1'=> '集团动态','2'=> '党建工作','3'=> '行业资讯','4'=> '下载中心');
-    $newsdata = M('news')->where("hide = 0 and type_id = $type")->limit(10)->select();
+    $newsdata = M('news')->where("hide = 0 and type_id = $type")->order('created_time desc')->limit(10)->select();
     if($get['id']){
       $id = (int)$get['id'];
       $anew = M('news')->find($id);
