361305c8f2c4bb8f9181d607bded4ef2c2f35bc1
Zhuxuan <zhuxuan@issuebang.com>
2016-12-12 02:05:52 +0800

    lgs-12-12-1

--- Lib/Action/AdminAction.class.php
+++ Lib/Action/AdminAction.class.php
@@ -132,4 +132,71 @@ class AdminAction extends Action {
     }
 	  $this->display();
   }
+
+  public function videos () {
+    if($_GET['hide']){
+      $hide = 1;
+    }else{
+      $hide = 0;
+    }
+    import('ORG.Util.Page');// 导入分页类
+    $count      = M('videos')->where('hide='.$hide)->count();// 查询满足要求的总记录数
+    $Page       = new Page($count, 20);// 实例化分页类 传入总记录数和每页显示的记录数
+    $show       = $Page->show();// 分页显示输出
+    $this->assign('list',$list);// 赋值数据集
+    $this->assign('page',$show);// 赋值分页输出
+
+    $storys = M('storys')->query('select gs_videos.*,(SELECT user from gs_user where id = gs_videos.created_by) as created_name, (SELECT user from gs_user where id = gs_videos.updated_by) as updated_name from gs_videos where hide = '. $hide .' order by setorder asc,created_time desc limit '.$Page->firstRow.',' .$Page->listRows);
+
+    //$storystype = array('0'=> 'My story','1'=> 'Your story','2'=> 'Our story');
+    $this->assign('storys',$storys);
+    //$this->assign('storystype',$storystype);
+    $this->assign('hide',$hide);
+    $this->display();
+  }
+
+  public function addvideo () {
+    if($this->_post()){
+      $p = $this->_post();
+      $video = M('videos');
+
+      $name = $_FILES['theme']['name'];
+      $time = time();
+      $str = Date("YmdHis", $time);
+      $image_name = 'gs' . $p['id'] . '-' . $str . '.' . end(explode('.', $name));
+      $tmp = $_FILES['theme']['tmp_name'];
+
+      $data = array();
+      $data["setorder"] = $p['setorder'];
+      $data["title"] = $p['title'];
+      $data["content"] = $p['content'];
+      if (move_uploaded_file($tmp, './UL/' . $image_name)) {
+        $data["theme"] = '/UL/' . $image_name;
+      }
+      if($p['id']){
+        $dash = $video->where('id = "'.$p['id']. '"')->find();
+        if($dash['theme']){
+          unlink('./UL/'.$dash['theme']);
+        }
+        $data["updated_by"] = $_SESSION['admin']['id'];
+        $data["updated_time"] = date("Y-m-d H:i:s");
+        $add = M('videos')->where('id = '.$p['id'])->save($data);
+        $this->success('更新成功');
+      }else{
+        $data["created_by"] = $_SESSION['admin']['id'];
+        $data["created_time"] = date("Y-m-d H:i:s");
+        $add = M('videos')->add($data);
+        $this->success('发布成功');
+      }
+      return;
+    }
+    if($this->_get()){
+      $p = (int)$_GET['id'];
+      $edit = M('videos')->find($p);
+      if($edit){
+        $this->assign('edit', $edit);
+      }
+    }
+	  $this->display();
+  }
 }
